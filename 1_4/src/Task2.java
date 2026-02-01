daa1214c853ee7e85ad8375532eb680642eb3b7d
jerep <jerepun@metropolia.fi>
2026-02-02 00:36:00 +0200

    Module 1_4 complete! Task 2 and 3 complete!

--- 1_4/src/Task2.java
+++ 1_4/src/Task2.java
@@ -1,2 +1,44 @@
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = input.nextInt();
+        int[] numbers = new int[size];
+        int[] startAndEnd = new int[2];
+        int sum = 0;
+        int subArrayLength = 2;
+
+        System.out.println("Enter the integers into the array:");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter integer " + (i + 1) + ": ");
+            numbers[i] = input.nextInt();
+        }
+
+        while (subArrayLength < numbers.length) {
+            for (int i = 0; i < numbers.length && i + subArrayLength <= numbers.length; i++) {
+                int subArrayEnd = i + subArrayLength;
+                int newSum = 0;
+                int[] subArray = new int[subArrayEnd - i];
+
+                for (int j = i; j < subArrayEnd; j++) {
+                    subArray[j - i] = numbers[j];
+                }
+                for (int k : subArray) {
+                    newSum += k;
+                }
+                if (newSum > sum) {
+                    startAndEnd[0] = i;
+                    startAndEnd[1] = subArrayEnd;
+                    sum = newSum;
+                }
+            }
+
+            subArrayLength++;
+        }
+
+        System.out.println("\nMaximum sum: " + sum);
+        System.out.println("Integers: " + (startAndEnd[0] + 1) + "-" + startAndEnd[1]);
+    }
 }
--- 1_4/src/Task3.java
+++ 1_4/src/Task3.java
@@ -6,25 +6,35 @@ public class Task3 {
         System.out.print("Enter the size of the array: ");
         int size = input.nextInt();
         int[] originalNumbers = new int[size];
-        int[] processedNumbers = new int[size];
+        int[] semiProcessedNumbers = new int[size];
+        int arrayPosition = 0;
 
         System.out.println("Enter the integers into the array:");
         for (int i = 0; i < size; i++) {
             System.out.print("Enter integer " + (i + 1) + ": ");
             originalNumbers[i] = input.nextInt();
         }
+
         for (int i = 0; i < originalNumbers.length; i++) {
             boolean isInArray = false;
-            for (int j = 0; j < processedNumbers.length; j++) {
-                if (originalNumbers[i] == processedNumbers[j]) {
+            for (int j = 0; j < semiProcessedNumbers.length; j++) {
+                if (originalNumbers[i] == semiProcessedNumbers[j]) {
                     isInArray = true;
+                    size--;
                     break;
                 }
             }
             if (!isInArray) {
-                processedNumbers[i] = originalNumbers[i];
+                semiProcessedNumbers[arrayPosition] = originalNumbers[i];
+                arrayPosition++;
             }
         }
+
+        int[] processedNumbers = new int[size];
+        for (int i = 0; i < size; i++) {
+            processedNumbers[i] = semiProcessedNumbers[i];
+        }
+
         for (int i = 0; i < processedNumbers.length; i++) {
             System.out.print(processedNumbers[i] + " ");
         }
